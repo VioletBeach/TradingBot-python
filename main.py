6e367b65eb8c525066b5a9533c5957f648dcd526
Violetbeach <ti6419@gmail.com>
2021-01-16 00:23:25 +0900

    Improve setting price

--- main.py
+++ main.py
@@ -10,13 +10,25 @@ f.close()
 
 bithumb = pybithumb.Bithumb(con_key, sec_key)
 
-# 코인별 구매 여부
+### 코인별 구매 가격 설정
+buy_soda_price=12.05
+buy_con_price=3.915
+buy_lamba_price=16.62
+buy_eforce_price=1326
+buy_queenB_price=5.102
+buy_ringX_price=32.22
+
+
+# 코인별 구매 여부 - 매수한 후에 판매하기 위해서
 is_buy_soda = 0
 is_buy_con = 0
 is_buy_lamba = 0
 is_buy_eforce = 0
 is_buy_queenB = 0
 is_buy_ringX = 0
+
+# 수익률
+yeild = 105/100
 
 while(1) :
 
@@ -28,68 +40,68 @@ while(1) :
     soda_price = pybithumb.get_current_price("SOC")
     if soda_price is not None:
         print("소다코인의 현재 가격 : ", soda_price)
-        if(soda_price<=12.05) :
+        if(soda_price<=buy_soda_price) :
             bithumb.buy_market_order("SOC", (krw/soda_price)*0.25)
             is_buy_soda=1
         
     con_price = pybithumb.get_current_price("CON")
     print("코넌의 현재 가격 : ", con_price)
     if con_price is not None:
-        if(con_price<=3.915) :
+        if(con_price<=buy_con_price) :
             bithumb.buy_market_order("CON", (krw/con_price)*0.25)
             is_buy_con=1
 
     lamba_price = pybithumb.get_current_price("LAMB")
     print("람바의 현재 가격 : ", lamba_price)
     if lamba_price is not None:
-        if(lamba_price<=16.62) :
+        if(lamba_price<=buy_lamba_price) :
             bithumb.buy_market_order("LAMB", (krw/lamba_price)*0.25)
             is_buy_lamba=1
         
     eforce_price = pybithumb.get_current_price("WOZX")
     print("이포스의 현재 가격 : ", eforce_price)
     if eforce_price is not None:
-        if(eforce_price<=1326) :
+        if(eforce_price<=buy_eforce_price) :
             bithumb.buy_market_order("WOZX", (krw/eforce_price)*0.25)
             is_buy_eforce=1
 
     queenB_price = pybithumb.get_current_price("QBZ")
     print("퀸비의 현재 가격 : ", queenB_price)
     if queenB_price is not None:
-        if(queenB_price<=5.102) :
+        if(queenB_price<=buy_queenB_price) :
             bithumb.buy_market_order("QBZ", (krw/queenB_price)*0.25)
             is_buy_queenB=1
         
     ringX_price = pybithumb.get_current_price("RINGX")
     print("링엑스의 현재 가격 : ", ringX_price)
     if ringX_price is not None:
-        if(ringX_price<=32.22) :
+        if(ringX_price<=buy_ringX_price) :
             bithumb.buy_market_order("RINGX", (krw/ringX_price)*0.25)
             is_buy_ringX=1
 
     # 매수한 코인들 모두 지정가 매수
     if(is_buy_soda==1) :
-        bithumb.sell_limit_order("SOC", 12.72 , bithumb.get_balance("SOC")[0]-bithumb.get_balance("SOC")[1])
+        bithumb.sell_limit_order("SOC", round(buy_soda_price*yeild,2) , bithumb.get_balance("SOC")[0]-bithumb.get_balance("SOC")[1])
         is_buy_soda=0
 
     if(is_buy_con==1) :
-        bithumb.sell_limit_order("CON", 4.114 , bithumb.get_balance("CON")[0]-bithumb.get_balance("CON")[1])
+        bithumb.sell_limit_order("CON", round(buy_con_price*yeild,3) , bithumb.get_balance("CON")[0]-bithumb.get_balance("CON")[1])
         is_buy_con=0
 
     if(is_buy_lamba==1) :
-        bithumb.sell_limit_order("LAMB", 17.43 , bithumb.get_balance("LAMB")[0]-bithumb.get_balance("LAMB")[1])
+        bithumb.sell_limit_order("LAMB", round(buy_lamba_price*yeild,2) , bithumb.get_balance("LAMB")[0]-bithumb.get_balance("LAMB")[1])
         is_buy_lamba=0
 
     if(is_buy_eforce==1) :
-        bithumb.sell_limit_order("WOZX", 1392 , bithumb.get_balance("WOZX")[0]-bithumb.get_balance("WOZX")[1])
+        bithumb.sell_limit_order("WOZX", int(buy_eforce_price*yeild) , bithumb.get_balance("WOZX")[0]-bithumb.get_balance("WOZX")[1])
         is_buy_eforce=0
 
     if(is_buy_queenB==1) :
-        bithumb.sell_limit_order("QBZ", 5.357 , bithumb.get_balance("QBZ")[0]-bithumb.get_balance("QBZ")[1])
+        bithumb.sell_limit_order("QBZ", round(buy_queenB_price*yeild,3) , bithumb.get_balance("QBZ")[0]-bithumb.get_balance("QBZ")[1])
         is_buy_queenB=0
 
     if(is_buy_ringX==1) :
-        bithumb.sell_limit_order("RINGX", 33.83 , bithumb.get_balance("SOC")[0]-bithumb.get_balance("SOC")[1])
+        bithumb.sell_limit_order("RINGX", round(buy_ringX_price*yeild,2) , bithumb.get_balance("SOC")[0]-bithumb.get_balance("SOC")[1])
         is_buy_ringX=0
     
 
