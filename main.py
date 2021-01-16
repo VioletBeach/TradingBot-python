6bb8d96ac1fd5b38973f4f8af93addb2f63d6a72
Violetbeach <ti6419@gmail.com>
2021-01-16 23:19:21 +0900

    update call API

--- main.py
+++ main.py
@@ -33,55 +33,58 @@ is_buy_proton = 0
 yeild = 105/100
 
 while(1) :
-
+    
     # 원화 자산 조회
     krw = bithumb.get_balance("BTC")[2]
     print("현재 잔고는", int(krw), "원입니다.\n")
 
+    # 호가 불러오기
+    all = pybithumb.get_orderbook("ALL")
+
     # 저점 달성시 매수 [저점을 변수로 설정할까 고민중]
-    soda_price = pybithumb.get_current_price("SOC")
+    soda_price = float(all['data']['SOC']['asks'][0]['price'])
     if soda_price is not None:
         print("소다코인의 현재 가격 : ", soda_price)
         if(soda_price<=buy_soda_price) :
             bithumb.buy_market_order("SOC", (krw/soda_price)*0.25)
             is_buy_soda=1
         
-    con_price = pybithumb.get_current_price("CON")
+    con_price = float(all['data']['CON']['asks'][0]['price'])
     print("코넌의 현재 가격 : ", con_price)
     if con_price is not None:
         if(con_price<=buy_con_price) :
             bithumb.buy_market_order("CON", (krw/con_price)*0.25)
             is_buy_con=1
 
-    lamba_price = pybithumb.get_current_price("LAMB")
+    lamba_price = float(all['data']['LAMB']['asks'][0]['price'])
     print("람바의 현재 가격 : ", lamba_price)
     if lamba_price is not None:
         if(lamba_price<=buy_lamba_price) :
             bithumb.buy_market_order("LAMB", (krw/lamba_price)*0.25)
             is_buy_lamba=1
         
-    eforce_price = pybithumb.get_current_price("WOZX")
+    eforce_price = float(all['data']['WOZX']['asks'][0]['price'])
     print("이포스의 현재 가격 : ", eforce_price)
     if eforce_price is not None:
         if(eforce_price<=buy_eforce_price) :
             bithumb.buy_market_order("WOZX", (krw/eforce_price)*0.25)
             is_buy_eforce=1
 
-    queenB_price = pybithumb.get_current_price("QBZ")
+    queenB_price = float(all['data']['QBZ']['asks'][0]['price'])
     print("퀸비의 현재 가격 : ", queenB_price)
     if queenB_price is not None:
         if(queenB_price<=buy_queenB_price) :
             bithumb.buy_market_order("QBZ", (krw/queenB_price)*0.25)
             is_buy_queenB=1
         
-    ringX_price = pybithumb.get_current_price("RINGX")
+    ringX_price = float(all['data']['RINGX']['asks'][0]['price'])
     print("링엑스의 현재 가격 : ", ringX_price)
     if ringX_price is not None:
         if(ringX_price<=buy_ringX_price) :
             bithumb.buy_market_order("RINGX", (krw/ringX_price)*0.25)
             is_buy_ringX=1
 
-    proton_price = pybithumb.get_current_price("XPR")
+    proton_price = float(all['data']['XPR']['asks'][0]['price'])
     print("프로톤의 현재 가격 : ", proton_price)
     if proton_price is not None:
         if(proton_price<=buy_proton_price) :
