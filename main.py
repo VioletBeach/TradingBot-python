595d503d136ca91b1fe7e3adf31d462bc36f11b1
Violetbeach <ti6419@gmail.com>
2021-01-17 00:21:11 +0900

    Modify order coin

--- main.py
+++ main.py
@@ -29,9 +29,18 @@ is_buy_queenB = 0
 is_buy_ringX = 0
 is_buy_proton = 0
 
+# 코인별 주문 변수 - 주문 존재 여부를 확인하기 위해서
+order_soda = 0
+order_con = 0
+order_lamba = 0
+order_eforce = 0
+order_queenB = 0
+order_ringX = 0
+order_proton = 0
+
+
 # 수익률
 yeild = 105/100
-
 while(1) :
     
     # 원화 자산 조회
@@ -45,51 +54,72 @@ while(1) :
     soda_price = float(all['data']['SOC']['asks'][0]['price'])
     if soda_price is not None:
         print("소다코인의 현재 가격 : ", soda_price)
-        if(soda_price<=buy_soda_price) :
-            bithumb.buy_market_order("SOC", (krw/soda_price)*0.25)
+        if(order_soda==0 and soda_price<=buy_soda_price*1.015) :
+            order_soda=bithumb.buy_limit_order("SOC", buy_soda_price, (krw/soda_price)*0.5)
             is_buy_soda=1
+        if(order_soda!=0 and soda_price>=buy_soda_price*1.035) :
+            bithumb.cancel_order(order_soda)
+            order_soda=0
         
     con_price = float(all['data']['CON']['asks'][0]['price'])
     print("코넌의 현재 가격 : ", con_price)
     if con_price is not None:
-        if(con_price<=buy_con_price) :
-            bithumb.buy_market_order("CON", (krw/con_price)*0.25)
+        if(order_con==0 and con_price<=buy_con_price*1.015) :
+            order_con=bithumb.buy_limit_order("CON", buy_con_price, (krw/con_price)*0.5)
             is_buy_con=1
+        if(order_con!=0 and con_price>=buy_con_price*1.035) :
+            bithumb.cancel_order(order_con)
+            order_con=0
 
     lamba_price = float(all['data']['LAMB']['asks'][0]['price'])
     print("람바의 현재 가격 : ", lamba_price)
     if lamba_price is not None:
-        if(lamba_price<=buy_lamba_price) :
-            bithumb.buy_market_order("LAMB", (krw/lamba_price)*0.25)
+        if(order_lamba==0 and lamba_price<=buy_lamba_price*1.015) :
+            bithumb.buy_limit_order("LAMB", buy_lamba_price, (krw/lamba_price)*0.5)
             is_buy_lamba=1
+        if(order_lamba!=0 and lamba_price>=buy_lamba_price*1.035) :
+            bithumb.cancel_order(order_lamba)
+            order_lamba=0
         
     eforce_price = float(all['data']['WOZX']['asks'][0]['price'])
     print("이포스의 현재 가격 : ", eforce_price)
     if eforce_price is not None:
-        if(eforce_price<=buy_eforce_price) :
-            bithumb.buy_market_order("WOZX", (krw/eforce_price)*0.25)
+        if(order_eforce==0 and eforce_price<=buy_eforce_price*1.015) :
+            bithumb.buy_limit_order("WOZX", buy_eforce_price, (krw/eforce_price)*0.5)
             is_buy_eforce=1
+        if(order_eforce!=0 and eforce_price>=buy_eforce_price*1.035) :
+            bithumb.cancel_order(order_eforce)
+            order_eforce=0
 
     queenB_price = float(all['data']['QBZ']['asks'][0]['price'])
     print("퀸비의 현재 가격 : ", queenB_price)
     if queenB_price is not None:
-        if(queenB_price<=buy_queenB_price) :
-            bithumb.buy_market_order("QBZ", (krw/queenB_price)*0.25)
+        if(order_queenB==0 and queenB_price<=buy_queenB_price*1.015) :
+            bithumb.buy_limit_order("QBZ", buy_queenB_price, (krw/queenB_price)*0.5)
             is_buy_queenB=1
+        if(order_queenB!=0 and queenB_price>=buy_queenB_price*1.035) :
+            bithumb.cancel_order(order_queenB)
+            order_queenB=0
         
     ringX_price = float(all['data']['RINGX']['asks'][0]['price'])
     print("링엑스의 현재 가격 : ", ringX_price)
     if ringX_price is not None:
-        if(ringX_price<=buy_ringX_price) :
-            bithumb.buy_market_order("RINGX", (krw/ringX_price)*0.25)
+        if(order_ringX==0 and ringX_price<=buy_ringX_price*1.015) :
+            bithumb.buy_limit_order("RINGX", buy_ringX_price, (krw/ringX_price)*0.5)
             is_buy_ringX=1
+        if(order_ringX!=0 and ringX_price>=buy_ringX_price*1.035) :
+            bithumb.cancel_order(order_ringX)
+            order_ringX=0
 
     proton_price = float(all['data']['XPR']['asks'][0]['price'])
     print("프로톤의 현재 가격 : ", proton_price)
     if proton_price is not None:
-        if(proton_price<=buy_proton_price) :
-            bithumb.buy_market_order("XPR", (krw/proton_price)*0.25)
+        if(order_proton==0 and proton_price<=buy_proton_price*1.015) :
+            bithumb.buy_limit_order("XPR", buy_proton_price, (krw/proton_price)*0.5)
             is_buy_proton=1
+        if(order_proton!=0 and proton_price>=buy_proton_price*1.035) :
+            bithumb.cancel_order(order_proton)
+            order_proton=0
 
     # 매수한 코인들 모두 지정가 매수
     if(is_buy_soda==1) :
